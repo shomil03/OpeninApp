89d024e03450745caa6483e424e923018643ba42
shomil03 <shomil02020@gmail.com>
2024-05-01 00:40:27 +0530

    Initial Commit

---
+++ OpeninApp/OpeninAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeninAppApp.swift
+//  OpeninApp
+//
+//  Created by Shomil Singh on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct OpeninAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
